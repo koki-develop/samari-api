5aae9c646f9e88f4b16fc0dcfa24f534a436c559
Koki Sato <kou.pg.0131@gmail.com>
2025-05-02 15:49:14 +0900

    fix: Set rate limit (#13)
    * fix: Set rate limit
    
    * chore: Add comment

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,16 @@ import { PostList } from "./endpoints/post/list";
 
 const app = new Hono<{ Bindings: Env }>();
 
+app.use(async (c, next) => {
+  const { success } = await c.env.RATE_LIMITER.limit({
+    key: c.req.header("x-forwarded-for") ?? "",
+  });
+  if (!success) {
+    return new Response("Rate limit exceeded", { status: 429 });
+  }
+  return next();
+});
+
 const openapi = fromHono(app, {
   docs_url: "/",
 });
